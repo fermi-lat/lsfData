eb6dbe0f936823aaf4d8432867c6c5ec22c6751e
ecephas <>
2008-12-09 00:08:50 +0000

    Check in lsfDataLib.py for lsfData v4r3 (branch-tag, GlastReleasev15r49).

--- lsfDataLib.py
+++ lsfDataLib.py
@@ -1,8 +1,8 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/lsfData/lsfDataLib.py,v 1.1 2008/07/09 21:13:47 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['lsfData'])
     env.Tool('eventFileLib')
-
+    env.Tool('addLibrary', library = env['ldfLibs'])
 def exists(env):
     return 1;
