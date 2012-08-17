2793381730612c20ae02136182346afb069b0e43
jrb <>
2012-08-17 18:46:51 +0000

    patches for Windows no-install-headers

--- lsfDataLib.py
+++ lsfDataLib.py
@@ -1,8 +1,12 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/lsfData/lsfDataLib.py,v 1.3 2009/01/23 00:20:59 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/lsfData/lsfDataLib.py,v 1.4 2009/08/25 23:24:21 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['lsfData'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'lsfData') 
     env.Tool('eventFileLib')
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums') 
     # env.Tool('addLibrary', library = env['ldfLibs'])
 def exists(env):
     return 1;
